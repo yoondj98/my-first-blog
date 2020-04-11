18ef3917cfc11c9213ebedd51b1b8c41f1c702c6
Xerath <yoondj98@naver.com>
2020-04-11 19:35:32 +0900

    회원가입 넘김 7

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,8 +21,6 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('accounts/login/', views.LoginView.as_view(), name='login'),
-    #path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
 
